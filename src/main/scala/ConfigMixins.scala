e4ee80d88679f766cc612e001a47875625ce77a9
Humail Nawaz <179082248+Humail-Nawaz@users.noreply.github.com>
2025-06-22 18:30:37 +0500

    Update ConfigMixins.scala

--- src/main/scala/ConfigMixins.scala
+++ src/main/scala/ConfigMixins.scala
@@ -1,31 +1,62 @@
 
+
 package serv
 
+
+
 import chisel3._
+
 import chisel3.util.{log2Up}
 
+
+
 import org.chipsalliance.cde.config.{Parameters, Config, Field}
+
 import freechips.rocketchip.subsystem._
+
 import freechips.rocketchip.rocket._
+
 import freechips.rocketchip.tile._
 
+
+
 /**
+
  * Create multiple copies of the Serv tile (and thus a core).
+
  * Override with the default mixins to control all params of the tiles.
+
  *
+
  * @param n amount of tiles to duplicate
+
  */
+
 class WithNServCores(n: Int = 1) extends Config((site, here, up) => {
+
   case TilesLocated(InSubsystem) => {
-    val prev = up(TilesLocated(InSubsystem), site)
+
+    val prev = up(TilesLocated(InSubsystem))
+
     val idOffset = up(NumTiles)
+
     (0 until n).map { i =>
+
       ServTileAttachParams(
+
         tileParams = ServTileParams(tileId = i + idOffset),
+
         crossingParams = RocketCrossingParams()
+
       )
+
     } ++ prev
+
   }
-  case SystemBusKey => up(SystemBusKey, site).copy(beatBytes = 4)
+
+ case SystemBusKey => up(SystemBusKey).copy(beatBytes = 4)
+
+
   case NumTiles => up(NumTiles) + n
+
 })
