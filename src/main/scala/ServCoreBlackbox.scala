207a802cacb4629bf19e9328cddf914bec00726a
Humail Nawaz <179082248+Humail-Nawaz@users.noreply.github.com>
2025-06-23 13:48:30 +0500

    Update ServCoreBlackbox.scala

--- src/main/scala/ServCoreBlackbox.scala
+++ src/main/scala/ServCoreBlackbox.scala
@@ -28,7 +28,7 @@ extends BlackBox(
    Map(
 	"MEMFILE_B"        -> StringParam(memfile_b),
 	"MEMSIZE_B"        -> IntParam(memsize_b),
-	"SIM_B"		   -> IntParam(sim_b),
+	"SIM_B"            -> IntParam(sim_b),
 	"RESET_STRATEGY_B" -> StringParam(reset_strategy_b),
 	"WITH_CSR_B"	   -> IntParam(with_csr_b),
 	"AW_B"             -> IntParam(aw_b),
@@ -45,10 +45,10 @@ extends BlackBox(
   		val  rst	 = Input(Bool())
   		val  i_timer_irq = Input(Bool())
 	  
-  		val  i_awaddr    = Input(UInt((aw_b-1).W))
+  		/*val  i_awaddr    = Input(UInt(aw_b.W))
 		val  i_awvalid   = Input(Bool())
 		val  o_awready   = Output(Bool())
-	        val  i_aw_id	 = Input(UInt((iw_b-1).W))
+	        val  i_aw_id	 = Input(UInt(iw_b.W))
 	        val  i_aw_len    = Input(UInt(8.W))
 	        val  i_aw_size   = Input(UInt(4.W))
 	        val  i_aw_burst  = Input(UInt(2.W))
@@ -58,12 +58,12 @@ extends BlackBox(
 	        val  i_aw_qos    = Input(UInt(4.W))
 	        val  i_aw_region = Input(UInt(4.W))
 	        val  i_aw_atop   = Input(UInt(6.W))
-	        val  i_aw_user   = Input(UInt((uw_b-1).W))
+	        val  i_aw_user   = Input(UInt(uw_b.W))
 	  
-		val  i_araddr    = Input(UInt((aw_b-1).W))
+		val  i_araddr    = Input(UInt(aw_b.W))
 		val  i_arvalid   = Input(Bool())
 		val  o_arready   = Output(Bool())
-                val  i_ar_id     = Input(UInt((iw_b-1).W))
+                val  i_ar_id     = Input(UInt(iw_b.W))
 	        val  i_ar_len    = Input(UInt(8.W))
 	        val  i_ar_size   = Input(UInt(4.W))
 	        val  i_ar_burst  = Input(UInt(2.W))
@@ -72,34 +72,34 @@ extends BlackBox(
 	        val  i_ar_prot   = Input(UInt(3.W))
 	        val  i_ar_qos    = Input(UInt(4.W))
 	        val  i_ar_region = Input(UInt(4.W))
-	        val  i_ar_user   = Input(UInt((uw_b-1).W))
+	        val  i_ar_user   = Input(UInt(uw_b.W))
 	  
 		val  i_wdata     = Input(UInt(32.W))
 		val  i_wstrb     = Input(UInt(4.W))
 		val  i_wvalid    = Input(Bool())
 		val  o_wready    = Output(Bool())
                 val  i_w_last    = Input(Bool())
-	        val  i_w_user    = Input(UInt((uw_b-1).W))
+	        val  i_w_user    = Input(UInt(uw_b.W))
 	  
 		val  i_bready    = Input(Bool())
 	        val  o_bresp     = Output(UInt(2.W))
 	        val  o_bvalid    = Output(Bool())
-                val  o_b_id      = Output(UInt((iw_b-1).W))
-	        val  o_b_user    = Output(UInt((uw_b-1).W))
+                val  o_b_id      = Output(UInt(iw_b.W))
+	        val  o_b_user    = Output(UInt(uw_b.W))
 	  
 	        val  i_rready    = Input(Bool())
 	        val  o_rdata     = Output(UInt(32.W))
 	        val  o_rresp     = Output(UInt(2.W))
 	  	val  o_rlast	 = Output(Bool())
 	  	val  o_rvalid    = Output(Bool())
-                val  o_r_id      = Output(UInt((iw_b-1).W))
-	        val  o_r_user    = Output(UInt((uw_b-1).W))
+                val  o_r_id      = Output(UInt(iw_b.W))
+	        val  o_r_user    = Output(UInt(uw_b.W))*/
 	  
 	  	val  i_awmready  = Input(Bool())
 	        val  o_awmaddr   = Output(UInt(12.W))
 	        val  o_awmvalid  = Output(Bool())
-                val  o_awm_id    = Output(UInt((iw_b-1).W)) 
-                val  o_awm_len   = Ouput(UInt(8.W))
+                val  o_awm_id    = Output(UInt(iw_b.W)) 
+                val  o_awm_len   = Output(UInt(8.W))
                 val  o_awm_size  = Output(UInt(3.W))
                 val  o_awm_burst = Output(UInt(2.W))
                 val  o_awm_lock  = Output(Bool())
@@ -108,13 +108,13 @@ extends BlackBox(
                 val  o_awm_qos   = Output(UInt(4.W))
                 val  o_awm_region= Output(UInt(4.W))
                 val  o_awm_atop  = Output(UInt(6.W))
-                val  o_awm_user  = Output(UInt((uw_b-1)).W)  
+                val  o_awm_user  = Output(UInt(uw_b.W)) 
 	  
 	        val  i_armready  = Input(Bool())
 	        val  o_armaddr   = Output(UInt(12.W))
 	  	val  o_armvalid  = Output(Bool())
-                val  o_arm_id    = Output(UInt((iw_b-1).W)) 
-                val  o_arm_len   = Ouput(UInt(8.W))
+                val  o_arm_id    = Output(UInt(iw_b.W)) 
+                val  o_arm_len   = Output(UInt(8.W))
                 val  o_arm_size  = Output(UInt(3.W))
                 val  o_arm_burst = Output(UInt(2.W))
                 val  o_arm_lock  = Output(Bool())
@@ -122,29 +122,29 @@ extends BlackBox(
                 val  o_arm_prot  = Output(UInt(3.W))
                 val  o_arm_qos   = Output(UInt(4.W))
                 val  o_arm_region= Output(UInt(4.W))
-                val  o_arm_user  = Output(UInt((uw_b-1)).W) 
+                val  o_arm_user  = Output(UInt(uw_b.W))
 	  
 	  	val  i_wmready   = Input(Bool())
 	  	val  o_wmdata	 = Output(UInt(32.W))
 	  	val  o_wmstrb    = Output(UInt(4.W))
 	  	val  o_wmvalid   = Output(Bool())
-                val  o_wm_last   = Ouput(Bool())
-                val  o_wm_user   = Output(UInt((uw_b-1)).W) 
+                val  o_wm_last   = Output(Bool())
+                val  o_wm_user   = Output(UInt(uw_b.W))
         
 	  
 	  	val  i_bmresp	 = Input(UInt(2.W))
 	  	val  i_bmvalid   = Input(Bool())
 	  	val  o_bmready   = Output(Bool())
-                val  i_bm_id     = Input(UInt((iw_b-1).W))
-                val  i_bm_user   = Input(UInt((uw_b-1)).W) 
+                val  i_bm_id     = Input(UInt(iw_b.W))
+                val  i_bm_user   = Input(UInt(uw_b.W))
 	  
 	  	val  i_rmdata    = Input(UInt(32.W))
 	  	val  i_rmresp    = Input(UInt(2.W))
 	  	val  i_rmlast	 = Input(Bool())
 	  	val  i_rmvalid   = Input(Bool())
 	  	val  o_rmready   = Output(Bool())
-	        val  i_rm_id     = Input(UInt((iw_b-1).W))
-                val  i_rm_user   = Input(UInt((uw_b-1)).W) 
+	        val  i_rm_id     = Input(UInt(iw_b.W))
+                val  i_rm_user   = Input(UInt(uw_b.W))
 }  )
 
 	  
