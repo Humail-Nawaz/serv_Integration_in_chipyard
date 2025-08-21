bbf9c30e6c3d7ce2cd624ced87e7d9520beb1183
Humail Nawaz <179082248+Humail-Nawaz@users.noreply.github.com>
2025-08-21 11:06:40 +0500

    Add files via upload

---
+++ chipyard.fpga.arty.Arty35THarness.ServArtyConfig/chipyard.fpga.arty.Arty35THarness.ServArtyConfig.bb.f
@@ -0,0 +1,6 @@
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/EICG_wrapper.v
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/GenericDigitalInIOCell.v
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/GenericDigitalOutIOCell.v
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/PowerOnResetFPGAOnly.v
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/ServCoreBlackbox.preprocessed.v
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/plusarg_reader.v
---
+++ chipyard.fpga.arty.Arty35THarness.ServArtyConfig/chipyard.fpga.arty.Arty35THarness.ServArtyConfig.model.f
@@ -0,0 +1,6 @@
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/plusarg_reader_Arty35THarness_UNIQUIFIED.v
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/AsyncResetSynchronizerPrimitiveShiftReg_d3_i0_Arty35THarness_UNIQUIFIED.sv
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/ResetCatchAndSync_d3_Arty35THarness_UNIQUIFIED.sv
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/AsyncResetSynchronizerShiftReg_w1_d3_i0_Arty35THarness_UNIQUIFIED.sv
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/Arty35THarness.sv
+/home/u20/chipyard/fpga/generated-src/chipyard.fpga.arty.Arty35THarness.ServArtyConfig/gen-collateral/PowerOnResetFPGAOnly.v
